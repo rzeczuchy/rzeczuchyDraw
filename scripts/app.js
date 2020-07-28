4252b015d2efd9a96ecb5d4d455e1c9b8225fc9d
rzeczuchy <57368722+rzeczuchy@users.noreply.github.com>
2020-07-28 11:11:26 +0200

    adding secure typing with ts

---
+++ scripts/app.js
@@ -0,0 +1,341 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+// CONSTANTS, VARIABLES, ENUMS
+var canvas = (document.getElementById("drawingCanvas"));
+var context = canvas.getContext("2d");
+var defaultCanvasWidth = 500;
+var defaultCanvasHeight = 400;
+var minCanvasSize = 1;
+var maxCanvasSize = 1000;
+var maxHistorySize = 3;
+var brushColorSelect = (document.getElementById("brushColorSelect"));
+var brushSizeSelect = (document.getElementById("brushSizeSelect"));
+var saveButton = (document.getElementById("saveButton"));
+var canvasWidthSelect = (document.getElementById("canvasWidthSelect"));
+var canvasHeightSelect = (document.getElementById("canvasHeightSelect"));
+var isDrawing = false;
+var isMouseDown = false;
+var x = 0;
+var y = 0;
+var brush;
+var colorPicker;
+var currentTool;
+var toUndo = [];
+var toRedo = [];
+var BrushShape;
+(function (BrushShape) {
+    BrushShape[BrushShape["Square"] = 0] = "Square";
+    BrushShape[BrushShape["Round"] = 1] = "Round";
+})(BrushShape || (BrushShape = {}));
+// EVENT LISTENERS
+canvas.addEventListener("mousedown", function (e) {
+    if (typeof currentTool !== "undefined" && currentTool != null) {
+        currentTool.onMouseDown(e);
+    }
+    isMouseDown = true;
+});
+canvas.addEventListener("mousemove", function (e) {
+    if (typeof currentTool !== "undefined" && currentTool != null) {
+        currentTool.onMouseMove(e);
+    }
+});
+canvas.addEventListener("mouseleave", function (e) {
+    if (typeof currentTool !== "undefined" && currentTool != null) {
+        currentTool.onMouseLeave(e);
+    }
+});
+canvas.addEventListener("mouseenter", function (e) {
+    if (typeof currentTool !== "undefined" && currentTool != null) {
+        currentTool.onMouseEnter(e);
+    }
+});
+window.addEventListener("mouseup", function (e) {
+    if (typeof currentTool !== "undefined" && currentTool != null) {
+        currentTool.onMouseUp(e);
+    }
+    isMouseDown = false;
+});
+window.addEventListener("keyup", function (e) {
+    handleKeyInput(e);
+});
+canvas.oncontextmenu = function (e) {
+    e.preventDefault();
+};
+window.onload = function () {
+    initialize();
+};
+// INTERFACES, CLASSES
+var Tool = /** @class */ (function () {
+    function Tool() {
+    }
+    Tool.prototype.onMouseDown = function (e) { };
+    Tool.prototype.onMouseMove = function (e) { };
+    Tool.prototype.onMouseLeave = function (e) { };
+    Tool.prototype.onMouseEnter = function (e) { };
+    Tool.prototype.onMouseUp = function (e) { };
+    return Tool;
+}());
+var Brush = /** @class */ (function (_super) {
+    __extends(Brush, _super);
+    function Brush() {
+        var _this = _super.call(this) || this;
+        _this.color = "#ff0066";
+        _this.minSize = 1;
+        _this.maxSize = 20;
+        _this.size = 1;
+        _this.shape = BrushShape.Square;
+        return _this;
+    }
+    Brush.prototype.onMouseDown = function (e) {
+        this.startDrawing(e);
+    };
+    Brush.prototype.onMouseMove = function (e) {
+        if (isDrawing) {
+            this.draw(e);
+        }
+    };
+    Brush.prototype.onMouseLeave = function (e) {
+        this.stopDrawing(e);
+    };
+    Brush.prototype.onMouseEnter = function (e) {
+        if (isMouseDown) {
+            this.startDrawing(e);
+        }
+    };
+    Brush.prototype.onMouseUp = function (e) {
+        if (isDrawing) {
+            this.stopDrawing(e);
+        }
+    };
+    Brush.prototype.startDrawing = function (e) {
+        saveCanvasState();
+        isDrawing = true;
+        x = e.offsetX;
+        y = e.offsetY;
+        this.drawPoint(x, y);
+    };
+    Brush.prototype.draw = function (e) {
+        this.drawLine(x, y, e.offsetX, e.offsetY);
+        x = e.offsetX;
+        y = e.offsetY;
+    };
+    Brush.prototype.stopDrawing = function (e) {
+        if (isDrawing) {
+            this.drawLine(x, y, e.offsetX, e.offsetY);
+            cacheImage();
+        }
+        x = 0;
+        y = 0;
+        isDrawing = false;
+    };
+    // Bresenham's algorithm in JS taken from this answer:
+    // https://stackoverflow.com/a/4672319/13352934
+    Brush.prototype.drawLine = function (x0, y0, x1, y1) {
+        var dx = Math.abs(x1 - x0);
+        var dy = Math.abs(y1 - y0);
+        var sx = x0 < x1 ? 1 : -1;
+        var sy = y0 < y1 ? 1 : -1;
+        var err = dx - dy;
+        while (true) {
+            this.drawPoint(x0, y0);
+            if (x0 === x1 && y0 === y1)
+                break;
+            var e2 = 2 * err;
+            if (e2 > -dy) {
+                err -= dy;
+                x0 += sx;
+            }
+            if (e2 < dx) {
+                err += dx;
+                y0 += sy;
+            }
+        }
+    };
+    Brush.prototype.drawPoint = function (x, y) {
+        switch (this.shape) {
+            case BrushShape.Square:
+                this.drawSquarePoint(x, y);
+                break;
+            case BrushShape.Round:
+                this.drawRoundPoint(x, y);
+                break;
+            default:
+                break;
+        }
+    };
+    Brush.prototype.drawSquarePoint = function (x, y) {
+        var halfBrush = Math.ceil(this.size / 2);
+        context.beginPath();
+        context.fillStyle = this.color;
+        context.fillRect(x - halfBrush, y - halfBrush, this.size, this.size);
+        context.fill();
+    };
+    Brush.prototype.drawRoundPoint = function (x, y) {
+        var halfBrush = Math.ceil(this.size / 2);
+        context.beginPath();
+        context.arc(x, y, halfBrush, 0, 2 * Math.PI);
+        context.fillStyle = this.color;
+        context.fill();
+    };
+    return Brush;
+}(Tool));
+var ColorPicker = /** @class */ (function (_super) {
+    __extends(ColorPicker, _super);
+    function ColorPicker() {
+        return _super.call(this) || this;
+    }
+    ColorPicker.prototype.onMouseDown = function (e) {
+        this.pickColor(e);
+    };
+    ColorPicker.prototype.onMouseMove = function (e) {
+        if (isMouseDown) {
+            this.pickColor(e);
+        }
+    };
+    ColorPicker.prototype.onMouseLeave = function (e) { };
+    ColorPicker.prototype.onMouseEnter = function (e) { };
+    ColorPicker.prototype.onMouseUp = function (e) {
+        currentTool = brush;
+    };
+    ColorPicker.prototype.pickColor = function (e) {
+        var imgData = context.getImageData(getCursorXPos(e), getCursorYPos(e), 1, 1);
+        brushColorSelect.value = getHexFromRgb(imgData.data[0], imgData.data[1], imgData.data[2]);
+        setBrushColor();
+    };
+    return ColorPicker;
+}(Tool));
+// FUNCTIONS
+var initialize = function () {
+    canvas.width = defaultCanvasWidth;
+    canvas.height = defaultCanvasHeight;
+    canvas.style.cursor = "crosshair";
+    clearToColor("#ffffff");
+    brush = new Brush();
+    colorPicker = new ColorPicker();
+    currentTool = brush;
+    brushColorSelect.value = brush.color.toString();
+    brushSizeSelect.value = brush.size.toString();
+    saveButton.setAttribute("download", "drawing.png");
+    canvasWidthSelect.value = canvas.width.toString();
+    canvasHeightSelect.value = canvas.height.toString();
+};
+var setBrushColor = function () {
+    brush.color = brushColorSelect.value;
+};
+var switchToColorPicker = function () {
+    currentTool = colorPicker;
+};
+var setBrushSize = function () {
+    brushSizeSelect.value = clamp(brushSizeSelect.value, brush.minSize, brush.maxSize).toString();
+    brush.size = parseInt(brushSizeSelect.value);
+};
+var setSquareBrush = function () {
+    brush.shape = BrushShape.Square;
+};
+var setRoundBrush = function () {
+    brush.shape = BrushShape.Round;
+};
+var cacheImage = function () {
+    saveButton.setAttribute("href", canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"));
+};
+var clearCanvas = function () {
+    if (confirm("This action will DESTROY your beautiful drawing! You sure?")) {
+        saveCanvasState();
+        clearToColor("#ffffff");
+    }
+};
+var setCanvasWidth = function () {
+    if (confirm("This action will clear the canvas. Are you sure?")) {
+        canvasWidthSelect.value = clamp(canvasWidthSelect.value, minCanvasSize, maxCanvasSize).toString();
+        canvas.width = parseInt(canvasWidthSelect.value);
+        clearToColor("#ffffff");
+        clearHistory();
+    }
+};
+var setCanvasHeight = function () {
+    if (confirm("This action will clear the canvas. Are you sure?")) {
+        canvasHeightSelect.value = clamp(canvasHeightSelect.value, minCanvasSize, maxCanvasSize).toString();
+        canvas.height = parseInt(canvasHeightSelect.value);
+        clearToColor("#ffffff");
+        clearHistory();
+    }
+};
+var saveCanvasState = function () {
+    var currentState = new Image();
+    currentState.src = canvas.toDataURL();
+    if (toUndo.length >= maxHistorySize) {
+        toUndo.splice(0, 1);
+    }
+    toUndo.push(currentState);
+    toRedo.length = 0;
+};
+var undo = function () {
+    if (typeof toUndo !== "undefined" && toUndo.length > 0) {
+        var currentState = new Image();
+        currentState.src = canvas.toDataURL();
+        toRedo.push(currentState);
+        context.drawImage(toUndo.pop(), 0, 0);
+    }
+};
+var redo = function () {
+    if (typeof toRedo !== "undefined" && toRedo.length > 0) {
+        var currentState = new Image();
+        currentState.src = canvas.toDataURL();
+        toUndo.push(currentState);
+        context.drawImage(toRedo.pop(), 0, 0);
+    }
+};
+var handleKeyInput = function (e) {
+    var charCode = e.charCode || e.keyCode;
+    switch (String.fromCharCode(charCode).toLowerCase()) {
+        case "z":
+            undo();
+            break;
+        case "y":
+            redo();
+            break;
+        default:
+            break;
+    }
+};
+var drawRectangle = function (x, y, width, height, color) {
+    context.beginPath();
+    context.fillStyle = color;
+    context.fillRect(x, y, width, height);
+    context.fill;
+};
+var clearToColor = function (color) {
+    drawRectangle(0, 0, canvas.width, canvas.height, color);
+};
+var clearHistory = function () {
+    toUndo.length = 0;
+    toRedo.length = 0;
+};
+var getCursorXPos = function (e) {
+    var rect = canvas.getBoundingClientRect();
+    return e.clientX - rect.left;
+};
+var getCursorYPos = function (e) {
+    var rect = canvas.getBoundingClientRect();
+    return e.clientY - rect.top;
+};
+var clamp = function (number, min, max) {
+    return Math.min(Math.max(number, min), max);
+};
+// convert rgb color to hex
+// from Stack Overflow https://stackoverflow.com/a/5623914/2849127
+var getHexFromRgb = function (r, g, b) {
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+};
--- scripts/app.ts
+++ scripts/app.ts
@@ -1,131 +1,150 @@
 "use strict";
 
-// CONSTANTS, VARIABLES
-const canvas = document.getElementById("drawingCanvas");
-const context = canvas.getContext("2d");
-const defaultCanvasWidth = 500;
-const defaultCanvasHeight = 400;
-const minCanvasSize = 1;
-const maxCanvasSize = 1000;
-const maxHistorySize = 3;
-const brushColorSelect = document.getElementById("brushColorSelect");
-const brushSizeSelect = document.getElementById("brushSizeSelect");
-const saveButton = document.getElementById("saveButton");
-const canvasWidthSelect = document.getElementById("canvasWidthSelect");
-const canvasHeightSelect = document.getElementById("canvasHeightSelect");
+// CONSTANTS, VARIABLES, ENUMS
+const canvas: HTMLCanvasElement = <HTMLCanvasElement>(
+  document.getElementById("drawingCanvas")
+);
+const context: CanvasRenderingContext2D = canvas.getContext("2d");
+const defaultCanvasWidth: number = 500;
+const defaultCanvasHeight: number = 400;
+const minCanvasSize: number = 1;
+const maxCanvasSize: number = 1000;
+const maxHistorySize: number = 3;
+const brushColorSelect: HTMLInputElement = <HTMLInputElement>(
+  document.getElementById("brushColorSelect")
+);
+const brushSizeSelect: HTMLTextAreaElement = <HTMLTextAreaElement>(
+  document.getElementById("brushSizeSelect")
+);
+const saveButton: HTMLButtonElement = <HTMLButtonElement>(
+  document.getElementById("saveButton")
+);
+const canvasWidthSelect: HTMLInputElement = <HTMLInputElement>(
+  document.getElementById("canvasWidthSelect")
+);
+const canvasHeightSelect: HTMLInputElement = <HTMLInputElement>(
+  document.getElementById("canvasHeightSelect")
+);
 
-let isDrawing = false;
-let isMouseDown = false;
-let x = 0;
-let y = 0;
-let brush;
-let colorPicker;
-let currentTool;
-let toUndo = [];
-let toRedo = [];
+let isDrawing: boolean = false;
+let isMouseDown: boolean = false;
+let x: number = 0;
+let y: number = 0;
+let brush: Brush;
+let colorPicker: ColorPicker;
+let currentTool: Tool;
+let toUndo: HTMLImageElement[] = [];
+let toRedo: HTMLImageElement[] = [];
+
+enum BrushShape {
+  Square,
+  Round,
+}
 
 // EVENT LISTENERS
-canvas.addEventListener("mousedown", (e) => {
+canvas.addEventListener("mousedown", (e: MouseEvent): void => {
   if (typeof currentTool !== "undefined" && currentTool != null) {
     currentTool.onMouseDown(e);
   }
   isMouseDown = true;
 });
 
-canvas.addEventListener("mousemove", (e) => {
+canvas.addEventListener("mousemove", (e: MouseEvent): void => {
   if (typeof currentTool !== "undefined" && currentTool != null) {
     currentTool.onMouseMove(e);
   }
 });
 
-canvas.addEventListener("mouseleave", (e) => {
+canvas.addEventListener("mouseleave", (e: MouseEvent): void => {
   if (typeof currentTool !== "undefined" && currentTool != null) {
     currentTool.onMouseLeave(e);
   }
 });
 
-canvas.addEventListener("mouseenter", (e) => {
+canvas.addEventListener("mouseenter", (e: MouseEvent): void => {
   if (typeof currentTool !== "undefined" && currentTool != null) {
     currentTool.onMouseEnter(e);
   }
 });
 
-window.addEventListener("mouseup", (e) => {
+window.addEventListener("mouseup", (e: MouseEvent): void => {
   if (typeof currentTool !== "undefined" && currentTool != null) {
     currentTool.onMouseUp(e);
   }
   isMouseDown = false;
 });
 
-window.addEventListener("keyup", (e) => {
+window.addEventListener("keyup", (e: KeyboardEvent): void => {
   handleKeyInput(e);
 });
 
-canvas.oncontextmenu = (e) => {
+canvas.oncontextmenu = (e: MouseEvent): void => {
   e.preventDefault();
 };
 
-window.onload = () => {
+window.onload = (): void => {
   initialize();
 };
 
-// CLASS DEFINITIONS
+// INTERFACES, CLASSES
 class Tool {
   constructor() {}
-  onMouseDown(e) {}
-  onMouseMove(e) {}
-  onMouseLeave(e) {}
-  onMouseEnter(e) {}
-  onMouseUp(e) {}
+  onMouseDown(e: MouseEvent) {}
+  onMouseMove(e: MouseEvent) {}
+  onMouseLeave(e: MouseEvent) {}
+  onMouseEnter(e: MouseEvent) {}
+  onMouseUp(e: MouseEvent) {}
 }
 
 class Brush extends Tool {
+  color: string;
+  minSize: number;
+  maxSize: number;
+  size: number;
+  shape: BrushShape;
+
   constructor() {
     super();
     this.color = "#ff0066";
     this.minSize = 1;
     this.maxSize = 20;
     this.size = 1;
-    this.shapes = {
-      SQUARE: "square",
-      ROUND: "round",
-    };
-    this.shape = this.shapes.SQUARE;
+    this.shape = BrushShape.Square;
   }
-  onMouseDown(e) {
+  onMouseDown(e: MouseEvent): void {
     this.startDrawing(e);
   }
-  onMouseMove(e) {
+  onMouseMove(e: MouseEvent): void {
     if (isDrawing) {
       this.draw(e);
     }
   }
-  onMouseLeave(e) {
+  onMouseLeave(e: MouseEvent): void {
     this.stopDrawing(e);
   }
-  onMouseEnter(e) {
+  onMouseEnter(e: MouseEvent): void {
     if (isMouseDown) {
       this.startDrawing(e);
     }
   }
-  onMouseUp(e) {
+  onMouseUp(e: MouseEvent): void {
     if (isDrawing) {
       this.stopDrawing(e);
     }
   }
-  startDrawing(e) {
+  startDrawing(e: MouseEvent): void {
     saveCanvasState();
     isDrawing = true;
     x = e.offsetX;
     y = e.offsetY;
     this.drawPoint(x, y);
   }
-  draw(e) {
+  draw(e: MouseEvent): void {
     this.drawLine(x, y, e.offsetX, e.offsetY);
     x = e.offsetX;
     y = e.offsetY;
   }
-  stopDrawing(e) {
+  stopDrawing(e: MouseEvent): void {
     if (isDrawing) {
       this.drawLine(x, y, e.offsetX, e.offsetY);
       cacheImage();
@@ -136,7 +155,7 @@ class Brush extends Tool {
   }
   // Bresenham's algorithm in JS taken from this answer:
   // https://stackoverflow.com/a/4672319/13352934
-  drawLine(x0, y0, x1, y1) {
+  drawLine(x0: number, y0: number, x1: number, y1: number): void {
     var dx = Math.abs(x1 - x0);
     var dy = Math.abs(y1 - y0);
     var sx = x0 < x1 ? 1 : -1;
@@ -157,26 +176,26 @@ class Brush extends Tool {
       }
     }
   }
-  drawPoint(x, y) {
+  drawPoint(x: number, y: number): void {
     switch (this.shape) {
-      case this.shapes.SQUARE:
+      case BrushShape.Square:
         this.drawSquarePoint(x, y);
         break;
-      case this.shapes.ROUND:
+      case BrushShape.Round:
         this.drawRoundPoint(x, y);
         break;
       default:
         break;
     }
   }
-  drawSquarePoint(x, y) {
+  drawSquarePoint(x: number, y: number): void {
     const halfBrush = Math.ceil(this.size / 2);
     context.beginPath();
     context.fillStyle = this.color;
     context.fillRect(x - halfBrush, y - halfBrush, this.size, this.size);
     context.fill();
   }
-  drawRoundPoint(x, y) {
+  drawRoundPoint(x: number, y: number): void {
     const halfBrush = Math.ceil(this.size / 2);
     context.beginPath();
     context.arc(x, y, halfBrush, 0, 2 * Math.PI);
@@ -189,20 +208,20 @@ class ColorPicker extends Tool {
   constructor() {
     super();
   }
-  onMouseDown(e) {
+  onMouseDown(e: MouseEvent): void {
     this.pickColor(e);
   }
-  onMouseMove(e) {
+  onMouseMove(e: MouseEvent): void {
     if (isMouseDown) {
       this.pickColor(e);
     }
   }
-  onMouseLeave(e) {}
-  onMouseEnter(e) {}
-  onMouseUp(e) {
+  onMouseLeave(e: MouseEvent): void {}
+  onMouseEnter(e: MouseEvent): void {}
+  onMouseUp(e: MouseEvent): void {
     currentTool = brush;
   }
-  pickColor(e) {
+  pickColor(e: MouseEvent): void {
     const imgData = context.getImageData(
       getCursorXPos(e),
       getCursorYPos(e),
@@ -219,7 +238,7 @@ class ColorPicker extends Tool {
 }
 
 // FUNCTIONS
-const initialize = () => {
+const initialize = (): void => {
   canvas.width = defaultCanvasWidth;
   canvas.height = defaultCanvasHeight;
   canvas.style.cursor = "crosshair";
@@ -227,80 +246,80 @@ const initialize = () => {
   brush = new Brush();
   colorPicker = new ColorPicker();
   currentTool = brush;
-  brushColorSelect.value = brush.color;
-  brushSizeSelect.value = brush.size;
+  brushColorSelect.value = brush.color.toString();
+  brushSizeSelect.value = brush.size.toString();
   saveButton.setAttribute("download", "drawing.png");
-  canvasWidthSelect.value = canvas.width;
-  canvasHeightSelect.value = canvas.height;
+  canvasWidthSelect.value = canvas.width.toString();
+  canvasHeightSelect.value = canvas.height.toString();
 };
 
-const setBrushColor = () => {
+const setBrushColor = (): void => {
   brush.color = brushColorSelect.value;
 };
 
-const switchToColorPicker = () => {
+const switchToColorPicker = (): void => {
   currentTool = colorPicker;
 };
 
-const setBrushSize = () => {
+const setBrushSize = (): void => {
   brushSizeSelect.value = clamp(
     brushSizeSelect.value,
     brush.minSize,
     brush.maxSize
-  );
-  brush.size = brushSizeSelect.value;
+  ).toString();
+  brush.size = parseInt(brushSizeSelect.value);
 };
 
-const setSquareBrush = () => {
-  brush.shape = brush.shapes.SQUARE;
+const setSquareBrush = (): void => {
+  brush.shape = BrushShape.Square;
 };
 
-const setRoundBrush = () => {
-  brush.shape = brush.shapes.ROUND;
+const setRoundBrush = (): void => {
+  brush.shape = BrushShape.Round;
 };
 
-const cacheImage = () => {
+const cacheImage = (): void => {
   saveButton.setAttribute(
     "href",
     canvas.toDataURL("image/png").replace("image/png", "image/octet-stream")
   );
 };
 
-const clearCanvas = () => {
+const clearCanvas = (): void => {
   if (confirm("This action will DESTROY your beautiful drawing! You sure?")) {
     saveCanvasState();
     clearToColor("#ffffff");
   }
 };
 
-const setCanvasWidth = () => {
+const setCanvasWidth = (): void => {
   if (confirm("This action will clear the canvas. Are you sure?")) {
     canvasWidthSelect.value = clamp(
       canvasWidthSelect.value,
       minCanvasSize,
       maxCanvasSize
-    );
-    canvas.width = canvasWidthSelect.value;
+    ).toString();
+    canvas.width = parseInt(canvasWidthSelect.value);
     clearToColor("#ffffff");
     clearHistory();
   }
 };
 
-const setCanvasHeight = () => {
+const setCanvasHeight = (): void => {
   if (confirm("This action will clear the canvas. Are you sure?")) {
     canvasHeightSelect.value = clamp(
       canvasHeightSelect.value,
       minCanvasSize,
       maxCanvasSize
-    );
-    canvas.height = canvasHeightSelect.value;
+    ).toString();
+    canvas.height = parseInt(canvasHeightSelect.value);
     clearToColor("#ffffff");
     clearHistory();
   }
 };
 
-const saveCanvasState = () => {
-  const currentState = new Image();
+const saveCanvasState = (): void => {
+  const currentState: HTMLImageElement = new Image();
   currentState.src = canvas.toDataURL();
 
   if (toUndo.length >= maxHistorySize) {
@@ -311,26 +330,26 @@ const saveCanvasState = () => {
   toRedo.length = 0;
 };
 
-const undo = () => {
+const undo = (): void => {
   if (typeof toUndo !== "undefined" && toUndo.length > 0) {
-    const currentState = new Image();
+    const currentState: HTMLImageElement = new Image();
     currentState.src = canvas.toDataURL();
     toRedo.push(currentState);
     context.drawImage(toUndo.pop(), 0, 0);
   }
 };
 
-const redo = () => {
+const redo = (): void => {
   if (typeof toRedo !== "undefined" && toRedo.length > 0) {
-    const currentState = new Image();
+    const currentState: HTMLImageElement = new Image();
     currentState.src = canvas.toDataURL();
     toUndo.push(currentState);
     context.drawImage(toRedo.pop(), 0, 0);
   }
 };
 
-const handleKeyInput = (e) => {
-  let charCode = event.charCode || event.keyCode;
+const handleKeyInput = (e: KeyboardEvent): void => {
+  let charCode: number = e.charCode || e.keyCode;
 
   switch (String.fromCharCode(charCode).toLowerCase()) {
     case "z":
@@ -344,38 +363,38 @@ const handleKeyInput = (e) => {
   }
 };
 
-const drawRectangle = (x, y, width, height, color) => {
+const drawRectangle = (x, y, width, height, color): void => {
   context.beginPath();
   context.fillStyle = color;
   context.fillRect(x, y, width, height);
   context.fill;
 };
 
-const clearToColor = (color) => {
+const clearToColor = (color: string): void => {
   drawRectangle(0, 0, canvas.width, canvas.height, color);
 };
 
-const clearHistory = () => {
+const clearHistory = (): void => {
   toUndo.length = 0;
   toRedo.length = 0;
 };
 
-const getCursorXPos = (e) => {
+const getCursorXPos = (e: MouseEvent): number => {
   const rect = canvas.getBoundingClientRect();
-  return event.clientX - rect.left;
+  return e.clientX - rect.left;
 };
 
-const getCursorYPos = (e) => {
+const getCursorYPos = (e: MouseEvent): number => {
   const rect = canvas.getBoundingClientRect();
-  return event.clientY - rect.top;
+  return e.clientY - rect.top;
 };
 
-const clamp = (number, min, max) => {
+const clamp = (number, min, max): number => {
   return Math.min(Math.max(number, min), max);
 };
 
 // convert rgb color to hex
 // from Stack Overflow https://stackoverflow.com/a/5623914/2849127
-const getHexFromRgb = (r, g, b) => {
+const getHexFromRgb = (r, g, b): string => {
   return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
 };
